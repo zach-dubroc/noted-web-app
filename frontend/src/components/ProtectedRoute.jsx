839b20972a82e116db8e27d0e6f0cb67d95a27ab
Zach Dubroc <zvd1993@gmail.com>
2024-04-10 19:46:53 -0500

    setup pages/routes

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,18 +2,31 @@
 import { Navigate } from "react-router-dom"
 import { jwtDecode } from "jwt-decode"
 import api from "../api"
-import { REFRESH_TOKEM, ACCESS_TOKEN } from "../constants"
-import { useState } from "react"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
+import { useState, useEffect } from "react"
 
 //check if authorized or redirect
 function ProtectedRoute({ children }) {
     const [isAuthorized, setIsAuthorized] = useState(null)
 
+    useEffect(()=>{
+        auth().catch(() => setIsAuthorized(false))
+    }, [])
     const refreshToken = async () => {
+        //gets refresh token
         const refreshToken = localStorage.getItem(REFRESH_TOKEN)
         try {
             const res = await api.post("api/token/refresh/", { refresh: refreshToken })
-        } catch (error) {
+            //check status code
+            if (res.status === 200){
+                localStorage.setItem(ACCESS_TOKEN, res.status.access)
+                setIsAuthorized(true)
+            } else {
+                setIsAuthorized(false)
+            }
+        } 
+        
+        catch (error) {
             setIsAuthorized(false)
         }
     }
@@ -48,4 +61,4 @@ export default ProtectedRoute
 
 
 
-//1:05:55
+
