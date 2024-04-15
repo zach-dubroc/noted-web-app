f8f0ae35145517f9103fa4a879e08424c788e99e
Zach Dubroc <zvd1993@gmail.com>
2024-04-15 17:54:47 -0500

    post to api

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,88 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
 
-function Home(){
-    return <div>Home</div>
+  //useEffect runs on initial render
+  //and on value change of 2nd argument(dependency)
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  //.then is like await but contains an error catch
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => err);
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) {
+          alert("note deleted");
+        } else alert("failed to delete");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) {
+          alert("note created");
+        } else alert("failed");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+      </div>
+
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <label htmlFor="content">Content</label>
+        <br />
+        <textarea
+          type="text"
+          id="Content"
+          name="Content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
