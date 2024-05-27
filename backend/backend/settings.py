433cb314996d15b37435530fc8ff552ab7a9a79a
ZachDubroc <zvd1993@gmail.com>
2024-05-27 11:34:25 -0500

    static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -159,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
